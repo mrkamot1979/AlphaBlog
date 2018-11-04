1b91e3060a63d82c1032e28d62982f0d5b271355
Niño Roldan <oninroldan@gmail.com>
2018-11-04 12:50:47 +0000

    Changed the minimum length of description to 10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,15 +5,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #render plain: params[:article].inspect this line shows the data that was inputted
+    #render plain: params[:article].inspect 
     @article = Article.new(article_params)
     @article.save
   end
   
   private
-  def article_params
-    params.require(:article).permit(:title, :description)
-  end
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
   
-
 end

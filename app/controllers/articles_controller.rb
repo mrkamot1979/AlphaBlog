fb1447325429109488e4bb0f652fc4622b5619dc
Niño Roldan <oninroldan@gmail.com>
2018-11-16 06:54:12 +0000

    Added def index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
-  end
+
+def index
+  
+end
+
+def new
+  @article = Article.new
+end
 
 def edit
   @article = Article.find(params[:id])

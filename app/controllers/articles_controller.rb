6798681f7882d149b48ab7fc16dd315f39dcc97d
Niño Roldan <oninroldan@gmail.com>
2018-11-16 02:01:57 +0000

    white space cleanup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,13 @@
 class ArticlesController < ApplicationController
   def new
     @article = Article.new
-    
   end
 
 def edit
   @article = Article.find(params[:id])
 end
 
-  def create #now with a handler that deals with article entry fails validation
+def create #now with a handler that deals with article entry fails validation
     #render plain: params[:article].inspect 
     @article = Article.new(article_params)
     if @article.save
@@ -17,28 +16,26 @@ end
     else
       render 'new'
     end
-    
+end
+  
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "Article successfully updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
   end
+end
   
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
-      flash[:notice] = "Article successfully updated"
-      redirect_to article_path(@article)
-    else
-      render 'edit'
-    end
+  
+def show
+  @article = Article.find(params[:id])
+end
+  
+private
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
-  
-  
-  def show
-    @article = Article.find(params[:id])
-    
-  end
-  
-  private
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
   
 end
